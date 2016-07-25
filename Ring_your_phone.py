73d2e6196cad2504a2888487f500e1b8ee91c522
Chetanya Chopra <chetanya221997chopra@gmail.com>
2016-07-25 00:18:08 -0700

    Ring_your_phone
    enter email rooted with your android device..........
    wait for few minutes after running....

--- Ring_your_phone.py
+++ Ring_your_phone.py
@@ -1,3 +1,5 @@
+
+
 import getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,15 +9,15 @@ password=getpass.getpass("password=")
 driver = webdriver.Firefox()
 driver.get("http://www.gmail.com")
 driver.implicitly_wait(5)
-driver.find_element_by_xpath("//*[@id='Email']").send_keys("dynamitechetan@gmail.com")
+driver.find_element_by_xpath("//*[@id='Email']").send_keys(user)
 driver.find_element_by_id("next").click()
-driver.find_element_by_xpath("//*[@id='Passwd']").send_keys("")
+driver.find_element_by_xpath("//*[@id='Passwd']").send_keys(password)
 driver.find_element_by_xpath("//*[@id='signIn']").click()
 driver.implicitly_wait(10)
 driver.get("https://myaccount.google.com/")
 driver.find_element_by_xpath("//*[@id='view_container']/div/div[2]/div/div[1]/a[3]/div/div[2]/div[2]/span").click()
 driver.find_element_by_xpath("//*[@id='view_container']/div/div/div[2]/div/div[2]/div[1]/div").click()
-driver.find_element_by_xpath("//*[@id='i2']/div[1]/div/div/div[2]/a[1]/div[1]").click()
+driver.find_element_by_xpath("//*[@id='i2']/div[1]/div/div/div[2]/a[2]/div[1]").click()
 #driver.find_element_by_xpath("//*[@id='gbwa']/div[1]/a").click()
 #driver.find_element_by_xpath('//*[@id="gb192"]/span[1]').click()
 #driver.implicitly_wait(10)
