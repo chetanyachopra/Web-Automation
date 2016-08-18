6f25b033a2a4ac15f8f6f63e3a4d25f780be23c4
Chetanya Chopra <chetanya221997chopra@gmail.com>
2016-08-18 10:56:14 +0530

    want to know which server a site uses??

--- get_server_name.py
+++ get_server_name.py
@@ -5,6 +5,6 @@ site =raw_input('enter site whose server u waane find= ')
 p=urllib2.urlopen("http://"+site)
 print p.headers.items()
 
-
+#find server name from headers
 '''from the info you are left with see for term server you will easily
  be able to see server name of site your site using '''
